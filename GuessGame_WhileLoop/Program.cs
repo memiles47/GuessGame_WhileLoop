a1555707f564dd5e24cc2c442b49fcd0f3781e60
Michael E Miles <michael@miles-me.net>
2017-07-24 03:37:03 -0500

    Comment out the basic steps required

--- GuessGame_WhileLoop/Program.cs
+++ GuessGame_WhileLoop/Program.cs
@@ -6,7 +6,14 @@ namespace GuessGame_WhileLoop
     {
         static void Main()
         {
+            //Create Random number between 1 and x
+
+
+            //Have player make a guess
             Console.WriteLine("Please guess a number between 1 and 10: ");
+
+
+            //Enter method for guessing in Guessing class
         }
     }
 }
