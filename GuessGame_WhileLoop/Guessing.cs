5e06d01bb3b6426665b7269b2951408dc9999554
Michael E Miles <michael@miles-me.net>
2017-07-24 14:11:58 -0500

    Finish guessing game using a class and method

--- GuessGame_WhileLoop/Guessing.cs
+++ GuessGame_WhileLoop/Guessing.cs
@@ -1,7 +1,30 @@
-﻿namespace GuessGame_WhileLoop
+﻿using System;
+
+namespace GuessGame_WhileLoop
 {
-    class Guessing(int guess)
+    internal static class Guessing
     {
-        
+        public static void Guesses(int guess, int answer)
+        {
+            while (guess != answer)
+            {
+                guess = Convert.ToInt32(Console.ReadLine());
+
+                if (guess == 0)
+                    break;
+
+                if (guess < answer)
+                    Console.Write("Please guess higher: ");
+
+                else if (guess > answer)
+                    Console.Write("Please guess lower: ");
+
+                else
+                    Console.Write("\nGreat job, you guessed correctly.");
+            }
+
+            Console.Write("\nPress any key to continue...");
+            Console.ReadKey();
+        }
     }
 }
--- GuessGame_WhileLoop/Program.cs
+++ GuessGame_WhileLoop/Program.cs
@@ -2,18 +2,20 @@
 
 namespace GuessGame_WhileLoop
 {
-    class Program
+    internal static class Program
     {
-        static void Main()
+        private static void Main()
         {
-            //Create Random number between 1 and x
+            var rnd = new Random();
 
+            Console.Write("Please enter the maximum answer you wish to guess: ");
+            var maxAnswer = Convert.ToInt32(Console.ReadLine());
+            var answer = rnd.Next(1, maxAnswer + 1);
 
-            //Have player make a guess
-            Console.WriteLine("Please guess a number between 1 and 10: ");
+            Console.Clear();
+            Console.Write($"Please enter your guess between 1 and {maxAnswer}, or 0 to exit: ");
 
-
-            //Enter method for guessing in Guessing class
+            Guessing.Guesses(answer + 1, answer);
         }
     }
 }
